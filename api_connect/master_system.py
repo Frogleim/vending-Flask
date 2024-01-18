2f7ff77c27e739512b48e02bc79ac892f88ba7a1
Gor B <92037197+Frogleim@users.noreply.github.com>
2024-01-18 03:54:14 -0800

    checkout added

--- api_connect/master_system.py
+++ api_connect/master_system.py
@@ -3,36 +3,34 @@ import socket
 
 
 def check_user(user_number, ip):
-    url = f'http://127.0.0.1:8000/check_user/?user_id={user_number}&ip_address={ip}'
+    url = f'http://192.168.1.2:8000/check_user/?user_id={user_number}&ip_address={ip}'
     r = requests.get(url)
     return r.json()
 
 
 def check_machine_status(ip):
-    url = f'http://127.0.0.1:8000/vending_status/?ip_address={ip}'
+    url = f'http://192.168.1.2:8000/vending_status/?ip_address={ip}'
     r = requests.get(url)
     return r.json()
 
 
 def get_ip_address():
     try:
-        # Create a socket object
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-        # Connect to an external server (doesn't actually send data)
         s.connect(("8.8.8.8", 80))
-
-        # Get the local IP address
         ip_address = s.getsockname()[0]
-
-        # Close the socket
         s.close()
-
         return ip_address
 
     except socket.error as e:
         print(f"Error: {e}")
         return None
+
+
+def checkout(user_id, product_id):
+    url = f'http://192.168.1.2:8000/checkout/?user_id={user_id}&product_id={product_id}'
+    r = requests.get(url)
+    return r.json()
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -1,24 +1,26 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 from api_connect import master_system
+from datetime import datetime, timedelta
+
 import socket
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+app.permanent_session_lifetime = timedelta(seconds=15)
 
 
 def get_ipv4_address():
-    try:
-        host_name = socket.gethostname()
-        print(host_name)
-        ip_address = socket.gethostbyname(host_name)
-        return ip_address
-    except socket.error as e:
-        print(f"Error: {e}")
-        return None
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]
 
 
 @app.route('/')
 def home():
     ip = get_ipv4_address()
+    user_id = request.form.get('user_id')
+    print(user_id)
+    print(ip)
     machine_status = master_system.check_machine_status(ip=ip)
     if 'success' in machine_status['status']:
         return render_template('index.html')
@@ -29,15 +31,31 @@ def home():
 @app.route('/process_form', methods=['POST'])
 def process_form():
     user_id = request.form.get('user_id')
-    ip_address = master_system.get_ip_address()
-    status = master_system.check_user(int(user_id), ip_address)
-    if 'success' in status['status']:
-        # Do something with user_id, e.g., save it to a database
-        return render_template('home.html', data=status['data'])
-    else:
-        return render_template('error.html')
+    session['user_id'] = user_id
+
+    # Check if user_id is in session, indicating an active session
+    if 'user_id' in session:
+        ip_address = master_system.get_ip_address()
+        status = master_system.check_user(int(user_id), ip_address)
+        data = {"master_system": status['data'], 'user_id': user_id}
+        if 'success' in status['status']:
+            # Do something with user_id, e.g., save it to a database
+            session['user_id'] = user_id
+            return render_template('home.html', data=data, user_id=user_id)
+
+    # If user_id is not in session or the check_user fails, redirect to the home page
+    return redirect(url_for('home'))
+
+
+@app.route('/takeout', methods=['POST'])
+def takeout_goods():
+    user_id = request.form.get('user_id')
+    product_id = request.form.get('product_id')
+    print(user_id, product_id)
+    return render_template('takeout.html')
 
 
 if __name__ == '__main__':
+    ip = get_ipv4_address()
     # Run the app on host 0.0.0.0 and port 5000
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host=f'{ip}', port=5000, debug=True)
